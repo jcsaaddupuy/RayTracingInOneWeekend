1492e3069a7c8064c6f7af9ce04a1df5c51fe826
jcsaaddupuy <saad.dupuy@gmail.com>
2025-01-04 22:07:14 +0100

    7.0 Moving Camera Code Into Its Own Class

---
+++ src/camera.rs
@@ -0,0 +1,96 @@
+use core::f64;
+use std::ffi::c_float;
+
+use indicatif::ProgressBar;
+
+use crate::color;
+use crate::color::Color;
+use crate::hittable::{HitRecord, Hittable};
+use crate::interval::Interval;
+use crate::ray::Ray;
+
+use crate::vec3::{Point3, Vec3};
+
+pub struct Camera {
+    pub aspect_ratio: f64, // Ratio of image width over height
+    pub image_width: i32,  // Rendered image width in pixel count
+
+    image_height: i32,   // Rendered image height
+    center: Point3,      // Camera center
+    pixel00_loc: Point3, // Location of pixel 0, 0
+    pixel_delta_u: Vec3, // Offset to pixel to the right
+    pixel_delta_v: Vec3, // Offset to pixel below
+}
+
+impl Camera {
+    pub fn new(image_width: i32, aspect_ratio: f64) -> Self {
+        // Calculate the image height, and ensure that it's at least 1.
+        let mut image_height = (image_width as f64 / aspect_ratio) as i32;
+        if image_height < 1 {
+            image_height = 1;
+        }
+
+        // Camera
+        let focal_length = 1.0;
+        let viewport_height = 2.0;
+        let viewport_width = viewport_height * (image_width as f64 / image_height as f64);
+        let center = Point3::new(0.0, 0.0, 0.0);
+
+        // Calculate the vectors across the horizontal and down the vertical viewport edges.
+        let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+        let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+
+        // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+        let pixel_delta_u = viewport_u / (image_width as f64);
+        let pixel_delta_v = viewport_v / (image_height as f64);
+
+        // Calculate the location of the upper left pixel.
+        let viewport_upper_left =
+            center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+
+        let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+        return Camera {
+            aspect_ratio,
+            image_width,
+            image_height,
+            center,
+            pixel00_loc,
+            pixel_delta_u,
+            pixel_delta_v,
+        };
+    }
+    pub fn ray_color<T: Hittable>(&self, r: Ray, world: &T) -> Color {
+        let mut rec: HitRecord = HitRecord::default();
+
+        if (world.hit(r, Interval::new(0.0, f64::INFINITY), &mut rec)) {
+            return 0.5 * (rec.normal + Color::new(1.0, 1.0, 1.0));
+        }
+
+        let unit_direction = r.direction().unit();
+        let a = 0.5 * (unit_direction.y() + 1.0);
+        return (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0);
+    }
+
+    pub fn render<T: Hittable>(&self, world: &T) {
+        let bar = ProgressBar::new((self.image_width * self.image_height) as u64);
+
+        print!("P3\n{} {}\n255\n", self.image_width, self.image_height);
+
+        for j in 0..self.image_height {
+            for i in 0..self.image_width {
+                let pixel_center = self.pixel00_loc
+                    + (i as f64 * self.pixel_delta_u)
+                    + (j as f64 * self.pixel_delta_v);
+                let ray_direction = pixel_center - self.center;
+                let r = Ray::new(self.center, ray_direction);
+
+                let pixel_color = self.ray_color(r, world);
+                color::write_color(pixel_color);
+
+                bar.inc(1);
+            }
+        }
+        bar.finish();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use core::f64;
 
+use camera::Camera;
 use color::Color;
 use hittable::{HitRecord, Hittable};
 use hittable_list::HittableList;
@@ -7,29 +8,18 @@ use indicatif::ProgressBar;
 use interval::Interval;
 use log::info;
 use ray::Ray;
-use shpere::Sphere;
+use sphere::Sphere;
 use vec3::{Point3, Vec3};
 
+mod camera;
 mod color;
 mod hittable;
 mod hittable_list;
 mod interval;
 mod ray;
 mod rtweekend;
-mod shpere;
+mod sphere;
 mod vec3;
-
-fn ray_color(r: Ray, world: &HittableList) -> Color {
-    let mut rec = HitRecord::default();
-
-    if world.hit(r, Interval::new(0.0, f64::INFINITY), &mut rec) {
-        return 0.5 * (rec.normal + Color::new(1.0, 1.0, 1.0));
-    }
-
-    let unit_direction = r.direction().unit();
-    let a = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0);
-}
 
 fn main() {
     colog::init();
@@ -37,11 +27,7 @@ fn main() {
     let aspect_ratio = 16.0 / 9.0;
     let image_width: i32 = 400;
 
-    // Calculate the image height, and ensure that it's at least 1.
-    let mut image_height = (image_width as f64 / aspect_ratio) as i32;
-    if image_height < 1 {
-        image_height = 1;
-    }
+    let camera = Camera::new(image_width, aspect_ratio);
     //World
 
     let mut world = HittableList::new();
@@ -49,43 +35,6 @@ fn main() {
     world.add(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5));
     world.add(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0));
 
-    // Camera
-    let focal_length = 1.0;
-    let viewport_height = 2.0;
-    let viewport_width = viewport_height * (image_width as f64 / image_height as f64);
-    let camera_center = Point3::new(0.0, 0.0, 0.0);
-
-    // Calculate the vectors across the horizontal and down the vertical viewport edges.
-    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
-    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
-
-    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-    let pixel_delta_u = viewport_u / (image_width as f64);
-    let pixel_delta_v = viewport_v / (image_height as f64);
-
-    // Calculate the location of the upper left pixel.
-    let viewport_upper_left =
-        camera_center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
-
-    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-    let bar = ProgressBar::new((image_width * image_height) as u64);
-
-    print!("P3\n{} {}\n255\n", image_width, image_height);
-
-    for j in 0..image_height {
-        for i in 0..image_width {
-            let pixel_center =
-                pixel00_loc + (i as f64 * pixel_delta_u) + (j as f64 * pixel_delta_v);
-            let ray_direction = pixel_center - camera_center;
-            let r = ray::Ray::new(camera_center, ray_direction);
-
-            let pixel_color = ray_color(r, &world);
-            color::write_color(pixel_color);
-
-            bar.inc(1);
-        }
-    }
-    bar.finish();
+    camera.render(&world);
     info!("Done");
 }
